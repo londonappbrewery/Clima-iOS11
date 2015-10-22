ce002c9c6e2ccd8d82d9693d07d89c223d774e28
angelabauer <angela_yu@hotmail.co.uk>
2015-10-22 22:00:07 +0100

    Starter Project

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -18,7 +18,8 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let LOCATION_URL = "http://openweathermap.org/data/2.5/forecast"
-    let CITY_URL = "http://api.openweathermap.org/data/2.5/weather?q=(name)&appid=bd82977b86bf27fb59a04b61b657fb6f"
+    let CITY_URL = "http://api.openweathermap.org/data/2.5/weather?q="
+    let APP_ID = "&appid=bd82977b86bf27fb59a04b61b657fb6f"
     
 
     //TODO: Declare some variables here.
