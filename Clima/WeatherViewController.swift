50c1a1e1b1a92b05d167d02de81fe5a3d690486d
mlutfiibra <mlutfiibra@outlook.com>
2019-10-16 21:05:35 +0700

    Adding location permission

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "4ff4e2fa6c87cdf3c077e6eb3b9bc719"
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -31,8 +31,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
     }
     
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+
+target 'Clima' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Clima
+    
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+    pod 'SVProgressHUD'
+
+end
